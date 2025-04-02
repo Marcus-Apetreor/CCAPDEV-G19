add84ca8b8ffa760880498982fc20ebbc776e0f8
Marcus-Apetreor <lee_jacob_apetreor@dlsu.edu.ph>
2025-04-02 19:57:11 +0800

    fix

--- controllers/server.js
+++ controllers/server.js
@@ -102,7 +102,7 @@ app.get("/", (req, res) => {
 async function createUsersFromSampleData() {
     try {
         // load sample data
-        const sampleData = JSON.parse(fs.readFileSync(path.join(__dirname, '.', 'sample_data', 'sample-users.json'), 'utf-8'));
+        const sampleData = JSON.parse(fs.readFileSync('sample_data/sample-users.json', 'utf-8'));
         for (const userData of sampleData) {
             const { username, email, password, tier, bio, profilePicture, approved } = userData;
 
@@ -147,8 +147,7 @@ function checkAuth(req, res, next) {
 async function createReservationsFromSampleData() {
     try {
         // load sample reservation data
-        const sampleData = JSON.parse(fs.readFileSync(path.join(__dirname, '.', 'sample_data', 'sample-reservations.json'), 'utf-8'));
-
+        const sampleData = JSON.parse(fs.readFileSync('sample_data/sample-reservations.json', 'utf-8'));
         for (const reservationData of sampleData) {
             const { room, roomType, date, timeslot, seat, username } = reservationData;
 
